251c797e1fc973038ebd69230d70c0c3b4bd733a
rfarah21 <rkhalil444@gmail.com>
2022-08-01 13:01:48 +0300

    Alpha

--- ResortManager1.0/Controllers/BookingController.cs
+++ ResortManager1.0/Controllers/BookingController.cs
@@ -29,11 +29,47 @@ namespace Resort_Management.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Choose(BookingModel obj)
         {
-            var test = obj.userbookedId;
+            //var test = obj.userbookedId;
             _db.Bookings.Add(obj);
             _db.SaveChanges();
             return View();
 
+        }
+
+        public IActionResult remove()
+        {
+            IEnumerable<BookingModel> objBookingList = _db.Bookings;
+            return View(objBookingList);
+        }
+
+        public IActionResult rmBooking(int? id)
+        {
+            var BookingFromDb = _db.Bookings.Find(id);
+            if (BookingFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(BookingFromDb);
+        }
+        [HttpPost, ActionName("rmBooking")]
+        [ValidateAntiForgeryToken]
+        public IActionResult rmBookingPOST(int? id)
+        {
+            var BookingFromDb = _db.Bookings.Find(id);
+            if (BookingFromDb == null)
+            {
+                return NotFound();
+            }
+
+/*            //get dropdown selected value and store in a variable 
+            var eventId = dropdown.SelectedValue;
+
+            //pass this variable in the GetDaysForEvents method to get DataSet.
+            var dataSet = GetDaysForEvents(eventId); Possible solution to the problem of using select tag*/
+
+            _db.Bookings.Remove(BookingFromDb);
+            _db.SaveChanges();
+            return RedirectToAction("Remove");
         }
         /*        [HttpPost]
                 [ValidateAntiForgeryToken]
